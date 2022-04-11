c479b2fcf0ff49f1fc169c2ac23c9132a66ab94e
Ammar saherwala <ammarsaherwala909@gmail.com>
2022-04-11 16:33:08 -0300

    First commit
    1st commit

---
+++ FinWiz/login/index.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FinWiz
+{
+    public partial class index1 : System.Web.UI.Page
+    {
+        FinWizService wizService = new FinWizService();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           // wizService.CheckLastPayroll("EMP20180001");
+        }
+
+        [System.Web.UI.Themeable(false)]
+        public virtual string ValidationGroup { get; set; }
+
+        protected void btn_login_Click(object sender, EventArgs e)
+        {
+            
+            string[] data = new string[3];
+            data[0] = txt_username.Text.ToString();
+            data[1] = txt_password.Text.ToString();
+            string[] result = wizService.get_user_login(data);
+            if (result[0].Equals("success"))
+            {
+                DefaultVar.usersession = result[1];
+                //DefaultVar.usersession = txt_username.Text;
+                DefaultVar.userrole = result[2];
+                Response.Redirect("../main.aspx");
+                
+            }
+            else
+            {
+                clearform();
+                ClientScript.RegisterStartupScript(Page.GetType(), "validation", "<script language='javascript'>alert('Please Enter Valid credential !!!')</script>");
+
+            }
+
+        }
+
+        private void clearform()
+        {
+           txt_username.Text = null;
+           txt_password.Text = null;
+        }
+
+        protected void btn_register_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Register.aspx");
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/login/forgetPassword.aspx");
+        }
+    }
+}
---
+++ FinWiz/master.Master.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FinWiz
+{
+    public partial class master : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var role = DefaultVar.userrole;
+
+            if (role.Equals("Employee"))
+            {
+                AddUser.Visible = false;
+                EditUser.Visible = false;
+                VerifyExpence.Visible = false;
+                Payroll.Visible = false;
+                GeneratePayroll.Visible = false;
+                navbarDropdown.Visible = false;
+
+            }
+        }
+
+        protected void AddExpense_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Expences/AddExpense.aspx");
+        }
+
+        protected void VerifyExpence_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Expences/VerifyExpense.aspx");
+        }
+
+        protected void ExpenceHistory_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Expences/HistoryExpense.aspx");
+        }
+
+        protected void Payroll_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Payroll/Payroll.aspx");
+        }
+
+        protected void GeneratePatroll_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Payroll/GeneratePayroll.aspx");
+        }
+
+        protected void PayrollHistory_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Payroll/PayrollHistory.aspx");
+        }
+
+        protected void AddUser_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Users/AddUser.aspx");
+        }
+
+        protected void EditUser_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Users/EditUser.aspx");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            DefaultVar.usersession = null;
+            Response.Redirect("~/login/index.aspx");
+        }
+    }
+}
